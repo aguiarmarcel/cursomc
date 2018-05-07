a108ca6ba50ea493d0bc2fa74008b119051d895f
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2018-05-07 20:09:47 -0300

    Testanto a primeira classe de domínio - Categoria

--- src/main/java/com/marcel/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/marcel/cursomc/resource/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.marcel.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcel.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value="/categorias")//esse nome será utilizado no browser. No plural, muito parecido com o cakephp
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	} 
 }

687f78b987cd113b13199938b98f001045700334
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2018-05-07 21:54:57 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/marcel/cursomc/domain/Categoria.java
+++ src/main/java/com/marcel/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.marcel.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	//Na sequência do pdf do professor tem o cheqlist para as entidades de domínio 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	

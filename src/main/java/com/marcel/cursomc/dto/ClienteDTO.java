7bd3e0c4f01f7334b26ff33237e87c03e08cf1a4
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2018-07-24 23:37:10 -0300

    PUT, DELETE e GET para cliente

---
+++ src/main/java/com/marcel/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,56 @@
+package com.marcel.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.marcel.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="Tamanho mínimo de 5 e máximo de 120 caracteres")
+	private String name;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
--- src/main/java/com/marcel/cursomc/services/ClienteService.java
+++ src/main/java/com/marcel/cursomc/services/ClienteService.java
@@ -1,11 +1,19 @@
 package com.marcel.cursomc.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcel.cursomc.domain.Cliente;
+import com.marcel.cursomc.dto.ClienteDTO;
 import com.marcel.cursomc.repositories.ClienteRepository;
+import com.marcel.cursomc.services.exceptions.DataIntegrityException;
 import com.marcel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +29,38 @@ public class ClienteService {
 					+ ", tipo: " +Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas. ");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer LinesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, LinesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newobj, Cliente obj) {
+		newobj.setName(obj.getName());
+		newobj.setEmail(obj.getEmail());
 	}
 }

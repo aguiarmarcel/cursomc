cecd5df9ca6eda4ff99b82bdd5fdf458d7828eae
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2018-08-21 12:00:52 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/marcel/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/marcel/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.marcel.cursomc.domain.Cliente;
+import com.marcel.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
